c8b32273eb37e8026324a860719ff6c06ee6562b
lina <nahyeon2252@gmail.com>
2025-04-23 19:06:39 +0900

    refactor : menu create refactoring

--- src/main/java/com/example/yamyam16/domain/menu/controller/MenuController.java
+++ src/main/java/com/example/yamyam16/domain/menu/controller/MenuController.java
@@ -27,4 +27,5 @@ public class MenuController {
 		MenuCreateResponseDto menuCreateResponseDto = menuService.menuCreate(storeId, menuCreateRequestDto); //dto
 		return new ResponseEntity<>(menuCreateResponseDto, HttpStatus.CREATED);
 	}
+
 }
--- src/main/java/com/example/yamyam16/domain/menu/service/MenuService.java
+++ src/main/java/com/example/yamyam16/domain/menu/service/MenuService.java
@@ -20,7 +20,8 @@ public class MenuService {
 		Long storeId,
 		MenuCreateRequestDto menuCreateRequestDto
 	) {
-		Store store = storeRepository.findById(storeId).orElseThrow(() -> new StoreNotFoundException("해당 가게가 없습니다."));
+		Store store = storeRepository.findById(storeId)
+			.orElseThrow(() -> new StoreNotFoundException("해당 가게가 없습니다.")); //해당 가게가 있는지 확인
 
 		Menu menu = new Menu(
 			menuCreateRequestDto.getMenuName(),
@@ -28,7 +29,7 @@ public class MenuService {
 		); // dto -> entiity로 변환 entity가 자바 클래스이니 객체로 생성해서 값을 보내서 해당 값으로 바꾼 Menu 클래스를 생성
 
 		Menu savedMenu = menuRepository.save(menu); //repository에 엔티티 보내서 데이터 베이스 테이블에 값 생성하고 그 값이 다시 반환됨 그래서 자료형이 Menu
-		return MenuCreateResponseDto();
+		return new MenuCreateResponseDto(savedMenu.getId(), savedMenu.getMenuName(), savedMenu.getMenuPrice());
 	}
 
 }
