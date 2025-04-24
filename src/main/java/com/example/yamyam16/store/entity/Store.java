699ef6d33de47d694e8a6474388d79bce95d8ca0
soyoonp <soyoonp@naver.com>
2025-04-25 05:51:57 +0900

    Feat : Crud 구현

--- src/main/java/com/example/yamyam16/store/entity/Store.java
+++ src/main/java/com/example/yamyam16/store/entity/Store.java
@@ -1,18 +1,11 @@
 package com.example.yamyam16.store.entity;
 
 import com.example.yamyam16.auth.entity.User;
-import com.example.yamyam16.domain.menu.entity.Menu;
+import com.example.yamyam16.menu.entity.Menu;
+import com.example.yamyam16.review.entity.Review;
+import com.example.yamyam16.store.dto.request.CreateStoreRequestDto;
+import com.example.yamyam16.store.dto.request.UpdateStoreRequestDto;
 import jakarta.persistence.*;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -25,39 +18,70 @@ import java.util.List;
 @Table(name = "store")
 public class Store {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@Column(nullable = false, length = 30)
-	private String name;
+    @Column(nullable = false, length = 30)
+    private String name;
 
-	@Column(nullable = false, length = 30)
-	private Long opentime;
+    @Column(nullable = false, length = 30)
+    private Long opentime;
 
-	@Column(nullable = false, length = 30)
-	private Long closetime;
+    @Column(nullable = false, length = 30)
+    private Long closetime;
 
     @Column(nullable = false, length = 30)
     private Long minprice;
 
-	private boolean isDelete;
+    private boolean isDelete;
 
-	@Column(nullable = false, length = 30)
-	private String category;
+    @Column(nullable = false, length = 30)
+    private String category;
 
-	@Column(nullable = false, length = 30)
-	private String notice;
+    @Column(nullable = false, length = 30)
+    private String notice;
 
-	//양방향 매핑
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "user_id")
-	private User user;
+    //양방향매핑관계
+    //유저
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 
-    // 자식 컬렉션을 가진 부모‑측 매핑
+    //리뷰
+    @OneToMany(mappedBy = "store",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true)
+    private List<Review> reviews = new ArrayList<>();
+
+    // 메뉴
     @OneToMany(mappedBy = "store",
             cascade = CascadeType.ALL,   // 부모 → 자식으로 영속성 전이
             orphanRemoval = true)        // 컬렉션에서 빠지면 자식 레코드 삭제
     private List<Menu> items = new ArrayList<>();
 
+
+    // 생성자
+    public Store(CreateStoreRequestDto dto, User user) {
+        this.name = dto.getName();
+        this.category = dto.getCategory();
+        this.notice = dto.getNotice();
+        this.minprice = dto.getMinOrderPrice();
+        this.opentime = dto.getOpen_time();
+        this.closetime = dto.getClose_time();
+        this.user = user;
+        this.isDelete = false;
+    }
+
+    //crud관련
+    public void update(UpdateStoreRequestDto updatePostsRequestDto) {
+        //todo 이거 고칠것
+    }
+
+    public void deactivate() {
+        this.isDelete = true;
+    }
+
 }
+
+
