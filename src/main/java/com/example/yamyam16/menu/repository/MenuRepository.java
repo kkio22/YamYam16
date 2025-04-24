1efaff5472242bb29a7b6f83bfcc11f008c2fc2e
harinbeee <luv_lyn@naver.com>
2025-04-24 16:44:21 +0900

    feat : 카트에서 사용 - 메뉴 찾는 메서드 추가

--- src/main/java/com/example/yamyam16/menu/repository/MenuRepository.java
+++ src/main/java/com/example/yamyam16/menu/repository/MenuRepository.java
@@ -1,12 +1,15 @@
-package com.example.yamyam16.domain.menu.repository;
+package com.example.yamyam16.menu.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.yamyam16.domain.menu.entity.Menu;
+import com.example.yamyam16.menu.entity.Menu;
+import com.example.yamyam16.store.entity.Store;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
 
 	Page<Menu> findAllByStoreId(Long storeId, Pageable pageable);
+
+	Menu findByMenuNameAndStore(String menuName, Store store);
 }
