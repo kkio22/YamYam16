aabc90bb7f70b7ad4e20dac8c395d2e2a43be6cc
soyoonp <soyoonp@naver.com>
2025-04-24 15:38:31 +0900

    Feat : 가게 CRUD 구현중

--- src/main/java/com/example/yamyam16/store/repository/StoreRepository.java
+++ src/main/java/com/example/yamyam16/store/repository/StoreRepository.java
@@ -1,11 +1,10 @@
 package com.example.yamyam16.store.repository;
 
-import com.example.yamyam16.auth.entity.User;
 import com.example.yamyam16.store.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
-    Optional<User> findById(Store store);
+    Optional<Store> findById(Store store);
 }
