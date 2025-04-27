1c036a70945251aee5d7bb33d29b19919acc5584
harinbeee <luv_lyn@naver.com>
2025-04-27 12:41:29 +0900

    fix : todto 메서드 내의 enum을 string으로 받을 수 있게 수정

--- src/main/java/com/example/yamyam16/store/dto/response/UpdateStoreResponseDto.java
+++ src/main/java/com/example/yamyam16/store/dto/response/UpdateStoreResponseDto.java
@@ -1,28 +1,28 @@
 package com.example.yamyam16.store.dto.response;
 
-import com.example.yamyam16.store.entity.CategoryType;
 import com.example.yamyam16.store.entity.Store;
+
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class UpdateStoreResponseDto {
-    private Long id;
-    private String storename;
-    private Long opentime;
-    private Long closetime;
-    private Long minOrderPrice;
-    private CategoryType category;
-    @Size(max = 100, message = "공지는 30글자 이하로 입력해주세요.")
-    private String notice;
+	private Long id;
+	private String storename;
+	private Long opentime;
+	private Long closetime;
+	private Long minOrderPrice;
+	private String category;
+	@Size(max = 100, message = "공지는 30글자 이하로 입력해주세요.")
+	private String notice;
 
-    public UpdateStoreResponseDto(Store store) {
-        this.id = store.getId();
-        this.storename = store.getName();
-        this.opentime = store.getOpenTime();
-        this.closetime = store.getCloseTime();
-        this.minOrderPrice = store.getMinOrderPrice();
-        this.category = CategoryType.valueOf(store.getCategory());
-        this.notice = store.getNotice();
-    }
+	public UpdateStoreResponseDto(Store store) {
+		this.id = store.getId();
+		this.storename = store.getName();
+		this.opentime = store.getOpenTime();
+		this.closetime = store.getCloseTime();
+		this.minOrderPrice = store.getMinOrderPrice();
+		this.category = store.getCategory().name();
+		this.notice = store.getNotice();
+	}
 }
