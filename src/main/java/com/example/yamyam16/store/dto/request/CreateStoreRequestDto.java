7299cd9ff90b90199992183bb2811de255045a68
harinbeee <luv_lyn@naver.com>
2025-04-27 16:20:44 +0900

    fix : Store 어노테이션 변경

--- src/main/java/com/example/yamyam16/store/dto/request/CreateStoreRequestDto.java
+++ src/main/java/com/example/yamyam16/store/dto/request/CreateStoreRequestDto.java
@@ -3,6 +3,7 @@ package com.example.yamyam16.store.dto.request;
 import com.example.yamyam16.store.entity.CategoryType;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,13 +15,13 @@ public class CreateStoreRequestDto {
 
 	@NotBlank(message = "상호명을 입력해주세요")
 	private String name;
-	@NotBlank(message = "개장시간을 입력해주세요")
-	private Long open_time;
-	@NotBlank(message = "마감시간을 입력해주세요")
-	private Long close_time;
-	@NotBlank(message = "최소주문금액을 입력해주세요")
+	@NotNull(message = "개장시간을 입력해주세요")
+	private Long opentime;
+	@NotNull(message = "마감시간을 입력해주세요")
+	private Long closetime;
+	@NotNull(message = "최소주문금액을 입력해주세요")
 	private Long minOrderPrice;
-	@NotBlank(message = "카테고리를 입력해주세요")
+	@NotNull(message = "카테고리를 입력해주세요")
 	private CategoryType category;
 	private String notice;
 
