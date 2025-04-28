1e10d1c58d581b69eff5742387e3d624f7f59e15
soyoonp <soyoonp@naver.com>
2025-04-28 17:24:18 +0900

    Feat : Crud 가게 수정

--- src/main/java/com/example/yamyam16/menu/controller/MenuController.java
+++ src/main/java/com/example/yamyam16/menu/controller/MenuController.java
@@ -1,70 +1,58 @@
 package com.example.yamyam16.menu.controller;
 
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.example.yamyam16.menu.dto.MenuCreateRequestDto;
 import com.example.yamyam16.menu.dto.MenuCreateResponseDto;
-import com.example.yamyam16.menu.dto.MenuListResponseDto;
 import com.example.yamyam16.menu.dto.MenuUpdateRequestDto;
 import com.example.yamyam16.menu.dto.MenuUpdateResponseDto;
 import com.example.yamyam16.menu.service.MenuService;
-
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class MenuController {
 
-	public final MenuService menuService;
+    public final MenuService menuService;
 
-	@PostMapping("/stores/{storeId}/menu")
-	public ResponseEntity<MenuCreateResponseDto> createMenu( //매서드명은 동사가 먼저와야 함
-		@PathVariable Long storeId, //지금 연관 관계된 가게의 url에 데이터 베이스 id 값이 있기에 그걸로 연관 지으면 됨
-		@Valid @RequestBody MenuCreateRequestDto menuCreateRequestDto
-	) {
-		MenuCreateResponseDto menuCreateResponseDto = menuService.createMenu(storeId, menuCreateRequestDto); //dto
-		return new ResponseEntity<>(menuCreateResponseDto, HttpStatus.CREATED);
-	}
+    @PostMapping("/stores/{storeId}/menu")
+    public ResponseEntity<MenuCreateResponseDto> createMenu( //매서드명은 동사가 먼저와야 함
+                                                             @PathVariable Long storeId, //지금 연관 관계된 가게의 url에 데이터 베이스 id 값이 있기에 그걸로 연관 지으면 됨
+                                                             @Valid @RequestBody MenuCreateRequestDto menuCreateRequestDto
+    ) {
+        MenuCreateResponseDto menuCreateResponseDto = menuService.createMenu(storeId, menuCreateRequestDto); //dto
+        return new ResponseEntity<>(menuCreateResponseDto, HttpStatus.CREATED);
+    }
 
-	@GetMapping("/stores/{storeId}/menu")
-	public ResponseEntity<List<MenuListResponseDto>> findMenuByPage(
-		@PathVariable Long storeId,
-		@RequestParam(defaultValue = "1") Long page, // 페이지 번호
-		@RequestParam(defaultValue = "10") Long size // 한페이지에 몇개의 데이터를 가져올지
-	) {//url 파라미터로 페이지 번호, 크기 받기
-		List<MenuListResponseDto> menuList = menuService.findMenuByPage(storeId, page, size);
-		return new ResponseEntity<>(menuList, HttpStatus.OK);
-	}
+//	@GetMapping("/stores/{storeId}/menu")
+//	public ResponseEntity<List<MenuListResponseDto>> findMenuByPage(
+//		@PathVariable Long storeId,
+//		@RequestParam(defaultValue = "1") Long page, // 페이지 번호
+//		@RequestParam(defaultValue = "10") Long size // 한페이지에 몇개의 데이터를 가져올지
+//	) {//url 파라미터로 페이지 번호, 크기 받기
+//		List<MenuListResponseDto> menuList = menuService.findMenuByPage(storeId, page, size);
+//		return new ResponseEntity<>(menuList, HttpStatus.OK);
+//	}
 
-	@PatchMapping("/stores/{storeId}/menu/{menuId}")// url이 달라야 함,
-	public ResponseEntity<MenuUpdateResponseDto> updateMenu(
-		@PathVariable Long storeId,
-		@PathVariable Long menuId,
-		@Valid @RequestBody MenuUpdateRequestDto menuUpdateRequestDto
-	) {
-		MenuUpdateResponseDto menuUpdateResponseDto = menuService.updateMenu(storeId, menuId, menuUpdateRequestDto);
-		return new ResponseEntity<>(menuUpdateResponseDto, HttpStatus.OK);
-	}
+    @PatchMapping("/stores/{storeId}/menu/{menuId}")// url이 달라야 함,
+    public ResponseEntity<MenuUpdateResponseDto> updateMenu(
+            @PathVariable Long storeId,
+            @PathVariable Long menuId,
+            @Valid @RequestBody MenuUpdateRequestDto menuUpdateRequestDto
+    ) {
+        MenuUpdateResponseDto menuUpdateResponseDto = menuService.updateMenu(storeId, menuId, menuUpdateRequestDto);
+        return new ResponseEntity<>(menuUpdateResponseDto, HttpStatus.OK);
+    }
 
-	@DeleteMapping("/stores/{storeId}/menu/{menuId}")
-	public ResponseEntity<Void> deleteMenu(
-		@PathVariable Long storeId,
-		@PathVariable Long menuId
-	) {
-		menuService.deleteMenu(storeId, menuId);
-		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-	}
+    @DeleteMapping("/stores/{storeId}/menu/{menuId}")
+    public ResponseEntity<Void> deleteMenu(
+            @PathVariable Long storeId,
+            @PathVariable Long menuId
+    ) {
+        menuService.deleteMenu(storeId, menuId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
 }
